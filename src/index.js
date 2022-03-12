311cec56d21f2240ed8e5b1b5d39b9b0255b5638
Rubens Algeri <rubens_zys@hotmail.com>
2022-03-12 14:21:23 -0300

    componentizao inicial e transformar em react o html

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import ReactDOM from 'react-dom';
+
+import NavBar from './NavBar';
+import Corpo from './Corpo';
+import Rodape from './Rodape';
+
+function App(){
+    return(
+        <div>
+            <NavBar />
+            <Corpo />
+            <Rodape />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
